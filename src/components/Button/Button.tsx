e94eb96ad59b46661bcc22d5aaceb4152f3ac026
Beatriz Siquara <beatrizsiquara@gmail.com>
2024-07-28 00:13:41 -0300

    Feat [ Layout de rotas e React Router DOM ]

--- src/components/Button/Button.tsx
+++
@@ -1,6 +0,0 @@
-import { ButtonContainer } from './Button.styles'
-import { ButtonProps } from './types'
-
-export function Button({ variant = 'primary' }: ButtonProps) {
-  return <ButtonContainer variant={variant}>Enviar</ButtonContainer>
-}
--- src/components/Button/types.ts
+++
@@ -1,9 +0,0 @@
-export type ButtonVariant = 'primary' | 'secondary' | 'danger' | 'success'
-
-export interface ButtonProps {
-  variant?: ButtonVariant
-}
-
-export interface ButtonContainerProps {
-  variant: ButtonVariant
-}
